64ba1006a3b7bc004188b7b6affa04d887b184e7
Oluwadamilola Cole <94747660+Avatecinnovations@users.noreply.github.com>
2025-04-21 09:31:50 +0100

    chore: Update ESLint configuration with Next.js rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,28 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import nextjs from "@next/eslint-plugin-next";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", ".next"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "plugin:@next/next/recommended",
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "@next/next": nextjs,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -24,6 +33,11 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-html-link-for-pages": "error",
+      "@next/next/no-img-element": "warn",
+      "@next/next/no-sync-scripts": "error",
+      "@next/next/no-typos": "error",
+      "@next/next/no-unwanted-polyfillio": "error",
     },
   }
 );
